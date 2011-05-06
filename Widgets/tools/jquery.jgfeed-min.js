fcae1c5e861b1bd798a770071d57e51dcb09c1a4
cadecairos <chris.decairos@gmail.com>
2011-05-06 12:24:21 -0400

    changed the getJSON call to ajax so that i can specify cache=false

--- Widgets/tools/jquery.jgfeed-min.js
+++ Widgets/tools/jquery.jgfeed-min.js
@@ -13,4 +13,31 @@
  *   http://me.boo.uz
  *
  */
-(function($){$.extend({jGFeed:function(url,fnk,num,key){if(url==null){return false;}var gurl="http://ajax.googleapis.com/ajax/services/feed/load?v=1.0&callback=?&q="+url;if(num!=null){gurl+="&num="+num;}if(key!=null){gurl+="&key="+key;}$.getJSON(gurl,function(data){if(typeof fnk=="function"){fnk.call(this,data.responseData.feed);}else{return false;}});}});})(jQuery);
+(function ($) {
+    $.extend({
+        jGFeed: function (url, fnk, num, key) {
+            if (url == null) {
+                return false;
+            }
+            var gurl = "http://ajax.googleapis.com/ajax/services/feed/load?v=1.0&callback=?&q=" + url;
+            if (num != null) {
+                gurl += "&num=" + num;
+            }
+            if (key != null) {
+                gurl += "&key=" + key;
+            }
+            $.ajax({
+              url: gurl,
+              cache: false,
+              dataType: 'json',
+              success: function (data) {
+                if (typeof fnk == "function") {
+                    fnk.call(this, data.responseData.feed);
+                } else {
+                    return false;
+                }
+            }
+          });
+        }
+    });
+})(jQuery);
