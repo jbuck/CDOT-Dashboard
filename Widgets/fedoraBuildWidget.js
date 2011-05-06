2d54243b0eb3a709f12946c348bb6edd7511c740
unknown <Chris@.(none)>
2011-05-05 23:49:48 -0400

    working fedoraBuildWidget

--- Widgets/fedoraBuildWidget.js
+++ Widgets/fedoraBuildWidget.js
@@ -2,61 +2,46 @@
 // 
 
 (function() {
-  
-  var machines = {
-    "0": {"name": "gnome-do-0.8.3.1-1.fc13", "state": "1"},
-    "1": {"name": "gcc-4.5.1-4.fc14", "state": "2"},
-    "2": {"name": "binutils-2.20.51.0.7-8.fc14", "state": "2"},
-    "3": {"name": "xorg-x11-drv-omapfb-0.1.1-1", "state": "1"},
-    "4": {"name": "binutils-2.20.51.0.7-4.fc14", "state": "3"},
-    "5": {"name": "asio-1.4.1-2.fc13", "state": "2"},
-    "6": {"name": "libtool-2.2.10-3.fc14", "state": "1"},
-    "7": {"name": "espeak-1.43-2.fc13", "state": "2"},
-    "8": {"name": "glibc-2.12.90-17", "state": "3"},
-    "9": {"name": "empathy-2.30.1-2.fc13", "state": "1"},
-    "10": {"name": "elfutils-0.152-1.fc14", "state": "1"},
-    "11": {"name": "gnome-do-0.8.3.1-1.fc13", "state": "3"}
-  };
-
   dashBoard.widget( "fedoraBuildWidget" , function( id, options ){
-
     var targetDiv = document.getElementById ( id );
-
-    if(targetDiv.className && options.cssClass) {
+    if (targetDiv.className && options.cssClass) {
       targetDiv.className = targetDiv.className + " " + options.cssClass;
     }
     else if (options.cssClass) {
       targetDiv.className = options.cssClass;
     }
-
-    var tableBorder     = options.border     ? options.border     : 0;
-    var tableHeight     = options.height     ? options.height     : "480px";
-    var tableWidth      = options.width      ? options.width      : "300px";
-    var tableClass      = options.tableClass ? options.tableClass : null;
-    
+    var tableBorder     = options.border      ? options.border      : 0;
+    var tableHeight     = options.height      ? options.height      : "480px";
+    var tableWidth      = options.width       ? options.width       : "300px";
+    var tableClass      = options.tableClass  ? options.tableClass  : null;
+    var cellSpacing     = options.cellSpacing ? options.cellSpacing : 0;
     var getBuildStatus = function() {
-      var items = [];
-      var states = { "1": "green", "2": "yellow", "3": "red"};
-
-      //for (var r = 0, i = 0; r < 3 && machines[i]; r++ ){
-        //items.push('<tr>');
-        for (var c = 0; c < 12 && machines[c]; c++){
-          items.push('<tr><td style="text-align: center; min-height: 30px; background: ' + states[machines[c].state] + '">' + machines[c].name + '</td></tr>');
+      var buildStats = [];
+      var states = { "complete": "green", "building": "yellow", "failed": "red", "cancelled": "orange"};
+      $.jGFeed('http://arm.koji.fedoraproject.org/koji/recentbuilds', function (response) {
+        if (!response) {
+            alert("Error");
+            return false;
         }
-        //items.push('</tr>');
-      //}
-
-      $('<table/>', {
-        html: items.join(''),
-        "border" : tableBorder,
-        "height" : tableHeight,
-        "width"  : tableWidth,
-        "class"  : tableClass
-      }).appendTo(targetDiv);        
-
+        for ( i = 0; i < response.entries.length; i++ ) {
+          var title = response.entries[i].title;
+          var outcome = title.split( ":", 2 )[0];
+          var build = title.split( ":", 2 );
+          build = build[1].split( ",", 2 )[0];
+          buildStats.push( '<tr><td style="text-align: center; min-height: 30px; background: ' + states[outcome] + '">' + build + '</td></tr>');
+        }
+        targetDiv.innerHTML = "";
+        $( '<table/>', {
+              html: buildStats.join(''),
+              "border"      : tableBorder,
+              "height"      : tableHeight,
+              "width"       : tableWidth,
+              "class"       : tableClass,
+              "cellspacing" : cellSpacing
+            }).appendTo(targetDiv);
+      },10);
     }
     getBuildStatus();
-    //window.setInterval ( getBuildStatus, 1000 );
+    window.setInterval ( getBuildStatus, 120000 );
   });
 } ());
-
