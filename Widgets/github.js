2e1fd32b802627e1dabc25387ee082def7187574
ScottDowne <sdowne@learn.senecac.on.ca>
2011-05-09 17:13:44 -0400

    changed url to jsonp request

--- Widgets/github.js
+++ Widgets/github.js
@@ -58,36 +58,31 @@
     // }
     // });
 
-    $.ajax({
-      type: "GET",
-      url: "http://github.com/api/v2/json/commits/list/" + userId + "/" + projectName + "/" + branchName,
-      dataType: "json",
-      success: function(result) {
-        var commits = result.commits,
-        uniqueDates = commits.arrayForKey('committed_date', normalizeDate).unique(),
-        arrayIdx;
+    $.getJSON( "http://github.com/api/v2/json/commits/list/" + userId + "/" + projectName + "/" + branchName + "?callback=?", function(result) {
+      var commits = result.commits,
+      uniqueDates = commits.arrayForKey('committed_date', normalizeDate).unique(),
+      arrayIdx;
 
-        //$('#' + id).append('<h1>Recent Commits</h1>').append('<p><b>' + commits.length + ' commits</b></p>');
+      //$('#' + id).append('<h1>Recent Commits</h1>').append('<p><b>' + commits.length + ' commits</b></p>');
 
-        for(arrayIdx = 0; arrayIdx < uniqueDates.length; arrayIdx += 1) {
-          var targetDiv = $('<div id="' + uniqueDates[arrayIdx].getTime() + '">');
-          //targetDiv.addClass('oneDayOfCommits');
-          $('#' + id).append(targetDiv);
-          targetDiv.append('<h2>' + uniqueDates[arrayIdx].toDateString() + '</h2>');
-          targetDiv.addClass('navcontainer');
-          targetDiv.append('<ul class="navlist"></ul>');
-          targetDiv.addClass(id);
-          targetDiv.addClass('scroll');
-        }
+      for(arrayIdx = 0; arrayIdx < uniqueDates.length; arrayIdx += 1) {
+        var targetDiv = $('<div id="' + uniqueDates[arrayIdx].getTime() + '">');
+        //targetDiv.addClass('oneDayOfCommits');
+        $('#' + id).append(targetDiv);
+        targetDiv.append('<h2>' + uniqueDates[arrayIdx].toDateString() + '</h2>');
+        targetDiv.addClass('navcontainer');
+        targetDiv.append('<ul class="navlist"></ul>');
+        targetDiv.addClass(id);
+        targetDiv.addClass('scroll');
+      }
 
-        // call the callback function if it is specified
-        options.callback && options.callback();
+      // call the callback function if it is specified
+      options.callback && options.callback();
 
-        for(arrayIdx = 0; arrayIdx < commits.length; arrayIdx += 1) {
-          var commit =  commits[arrayIdx],
-          committedDate = normalizeDate(commit.committed_date);
-          $('#' + committedDate.getTime() + '> ul').append('<li>' + commit.author.name + ' committed with message "' + commit.message + '"</li>');
-        }
+      for(arrayIdx = 0; arrayIdx < commits.length; arrayIdx += 1) {
+        var commit =  commits[arrayIdx],
+        committedDate = normalizeDate(commit.committed_date);
+        $('#' + committedDate.getTime() + '> ul').append('<li>' + commit.author.name + ' committed with message "' + commit.message + '"</li>');
       }
     });
   });
