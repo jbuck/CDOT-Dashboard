55beb50e3399402dd55cd395cbecf16049f02f59
cadecairos <chris.decairos@gmail.com>
2011-05-09 18:06:27 -0400

    changed some files. added blog and fedora widget to geckoboard.html

--- Widgets/blogReaderWidget.js
+++ Widgets/blogReaderWidget.js
@@ -2,13 +2,13 @@
 (function () {
 
   
-  dashBoard.widget( "blogFeed" , function( options ) {
+  dashBoard.widget( "blogReaderWidget" , function( options ) {
     // create a new div and append it to the parent div so nothing
     // that already exists in the parent div gets overwritten
     var newdiv = document.createElement( "div" );
     newdiv.id = "_feed";
     newdiv.style.width = "100%";
-    newdiv.style.height = "100%";
+    newdiv.style.height = "430px";
 
     if ( document.getElementById( options.target ) ) {
       document.getElementById( options.target ).appendChild( newdiv );
@@ -16,12 +16,24 @@
 
     var initialize = function() {
       // Create the feed control using the user entered url and title
-      new GFdynamicFeedControl( options.url, newdiv, {
+      
+      var gf = new GFdynamicFeedControl( options.url, newdiv, {
         vertical: options.orientation.toLowerCase() == "vertical" ? true : false,
         horizontal: options.orientation.toLowerCase() == "horizontal" ? true : false,
         title: options.title = options.title || "Blog"
-      });
+      });  
       
+      var setHeight = function() { 
+        var d = document.getElementsByClassName('gfg-entry');
+        if (d.length == 0) {
+          setTimeout(function() { 
+            setHeight();
+          }, 5);
+        } else {
+          d[0].style.height = "270px";       
+        }
+      }
+      setHeight();
     };
     
     initialize();
