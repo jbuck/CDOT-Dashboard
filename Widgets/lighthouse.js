d9ad065b3d0f3aedfcb9c3857f0ddf76eec006b1
David Seifried <david.c.seifried@gmail.com>
2011-05-09 16:12:57 -0400

    Changed lighthouse api yet again, made some css stuff

--- Widgets/lighthouse.js
+++ Widgets/lighthouse.js
@@ -9,7 +9,7 @@
 
 		// JQuery used to get the JSON data from our server, values stored in arrays named accordingly
 		$.getJSON(
-			"http://scotland.proximity.on.ca/sdowne/sqlite/jsonp.php?callback=?&service=lighthouse",
+			"http://scotland.proximity.on.ca/sdowne/sqlite/jsonp.php?callback=?&service=lighthouse-popcorn",
 			function(json) {
 			
 				open = new Array(json.milestones.length);
@@ -23,64 +23,70 @@
 					open[i] = json.milestones[i].milestone.open_tickets_count;
 					closed[i] = json.milestones[i].milestone.tickets_count - json.milestones[i].milestone.open_tickets_count;
 				}
-				
+
 				finished();
 		});
 
 		// Callback to be called once all JSON data has been loaded
 		function finished(){
 		
-		dashBoard.milestone( options.timeDiv, {
-            what: title[options.milestone-1],
-            when: dueOn[options.milestone-1],
-            gradient: "days"
-          });
-			
-			// Create new Google Visualization data table
-			var data = new google.visualization.DataTable();				
-				
-			// Check to make sure a milestone was entered, if not, default to last milestone
-			options.milestone = options.milestone || open.length-1;
-				
-			// Store the data for all the open and closed tickets
-			var raw_data = [['Open Tickets', open[options.milestone-1]],
-											['Closed Tickets', closed[options.milestone-1]]];
-				
-			data.addColumn('string', 'Ticket');
-			
-			for (var i = 0; i  < raw_data.length; ++i) {
-				data.addColumn('number', raw_data[i][0]);    
-			}   
-  
-			data.addRows(1);
-				
-			// Set the values for all the ticket data
-			for (var i = 0; i  < raw_data.length; ++i) {
-				for (var j = 1; j  < raw_data[i].length; ++j) {
-					data.setValue(j-1, i+1, raw_data[i][j]);    
-				}
-			}
- 
-			// Create and draw the visualization, as well as set all options
-			
-			new google.visualization.BarChart(document.getElementById(id)).
-			draw(data,{
-				width:400, height:200,
-				title: 					 "Milestone: " + title[options.milestone-1],
-				titleTextStyle:	 {color:"#000000", fontSize:24},
-				isStacked: 			 true,
-				chartArea:			 {width:"100%"},
-				showValueLabels: false,
-				backgroundColor: "#FFFFFF",
-				gridlineColor:   "#FFFFFF",
-				hAxis:					 {baselineColor: "#FFFFFF", textStyle:{color:"#FFFFFF"}},
-				legend: 				 "none",
-				vAxis:					 {baselineColor: "#FFFFFF"},
-				colors:					 ['red','green']
-			})
+		var newdiv, newdivz, width, words, words2, oText, oText2, names = "div" + options.milestone, namez = "divz" + options.milestone;
+		if(!document.getElementById(names)){
+			newdiv = document.createElement('div');
+			newdivz = document.createElement('div');
+			newdivz.id = "divz" + options.milestone;
+			newdiv.id = "div" + options.milestone;
 		}
+		else{
+			newdiv.id = names;
+			newdivz.id = namez;
+		}
+		
+		if(open[options.milestone-1] == 0 || closed[options.milestone-1] == 0){
+			open[options.milestone-1] == 0 ? width = "100%" : width = "0%";
+		}
+		else{
+			width = (open[options.milestone-1]/(closed[options.milestone-1]+open[options.milestone-1]));
+			width = 100 - (width*100) + "%";
+		}
+		
+		words = document.createElement("p");
+		words2 = document.createElement("p");
+      		words.innerHTML = "<strong><b>" + open[options.milestone-1] + "</b></strong> open tickets";
+		words2.innerHTML = "<b><strong>" + closed[options.milestone-1] +  "</strong></b> closed tickets";
+		document.getElementById(id).appendChild(words);
+		document.getElementById(id).appendChild(words2);
 
+		newdivz.width = "200px";
+		newdivz.height = "50px";
+		newdivz.align="centre";
+		newdivz.style.backgroundColor = "grey";
+		newdivz.style.border = "2px solid black";
+		newdivz.style.width = "200px";
+		newdivz.style.height = "25px";
+
+		newdiv.style.backgroundColor = "green";
+		newdiv.style.width = width;
+		newdiv.style.height = newdivz.style.height;
+		newdiv.innerHTML = "&nbsp;";
+		document.getElementById(id).appendChild(newdivz);
+		newdivz.appendChild(newdiv);
+
+		newdiv_ = document.createElement("div");
+		newdiv_.id = "times" + options.milestone-1;
+		document.getElementById(id).appendChild(newdiv_);
+				
+
+		if(options.time){
+			dashBoard.milestone( newdiv_.id, {
+           		what: title[options.milestone-1],
+            		when: dueOn[options.milestone-1],
+            		gradient: "days"
+          	});
+		}
+				
 		setTimeout( refresh, 30000 );
+}
 	};
 	refresh();
 	});
